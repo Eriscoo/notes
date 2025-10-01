1ceac08393ccf0ed56f281151e36aa8c3d637c53
Erisco Berto <erisco@localhost.localdomain>
2025-10-01 23:42:14 +0700

    Update notes

---
+++ reusepalindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class reusepalindrome {
+    
+    // Reusable function untuk cek palindrome
+    public static boolean isPalindrome(String text) {
+        return text.equals(new StringBuilder(text).reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner textInput = new Scanner(System.in);
+        System.out.print("Masukkan kata, frasa atau angka: ");
+        String text = textInput.nextLine();
+
+        // Gunakan reusable function
+        if (isPalindrome(text)) {
+            System.out.println(text + " adalah Palindrome.");
+        } else {
+            System.out.println(text + " bukanlah Palindrome.");
+        }
+
+        textInput.close();
+    }
+}
